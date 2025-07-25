a82d5d33803c34f1714404a740dec8e80cac43d7
Luna-bf <lburattifayolle@gmail.com>
2025-07-25 15:39:29 +0200

    Added a function for the carousel + elements in objects

--- js/main.js
+++ js/main.js
@@ -1,46 +1,73 @@
 /***OBJECTS***/
-const products = [
+
+// Images
+const productsImages = [
     {
         id: 1,
-        productImageSrc: './ecommerce-product-page-main/ecommerce-product-page-main/images/image-product-1.jpg',
         productThumbnailSrc: './ecommerce-product-page-main/ecommerce-product-page-main/images/image-product-1-thumbnail.jpg',
+        productThumbnailAlt: 'Product 1 (thumbnail)',
+        productImageSrc: './ecommerce-product-page-main/ecommerce-product-page-main/images/image-product-1.jpg',
+        productImageAlt: 'Product 1',
     },
     {
         id: 2,
-        productImageSrc: './ecommerce-product-page-main/ecommerce-product-page-main/images/image-product-2.jpg',
         productThumbnailSrc: './ecommerce-product-page-main/ecommerce-product-page-main/images/image-product-2-thumbnail.jpg',
+        productThumbnailAlt: 'Product 2 (thumbnail)',
+        productImageSrc: './ecommerce-product-page-main/ecommerce-product-page-main/images/image-product-2.jpg',
+        productImageAlt: 'Product 2',
     },
     {
         id: 3,
-        productImageSrc: './ecommerce-product-page-main/ecommerce-product-page-main/images/image-product-3.jpg',
         productThumbnailSrc: './ecommerce-product-page-main/ecommerce-product-page-main/images/image-product-3-thumbnail.jpg',
+        productThumbnailAlt: 'Product 3 (thumbnail)',
+        productImageSrc: './ecommerce-product-page-main/ecommerce-product-page-main/images/image-product-3.jpg',
+        productImageAlt: 'Product 3 (image)',
     },
     {
         id: 4,
-        productImageSrc: './ecommerce-product-page-main/ecommerce-product-page-main/images/image-product-4.jpg',
         productThumbnailSrc: './ecommerce-product-page-main/ecommerce-product-page-main/images/image-product-4-thumbnail.jpg',
+        productThumbnailAlt: 'Product 4 (thumbnail)',
+        productImageSrc: './ecommerce-product-page-main/ecommerce-product-page-main/images/image-product-4.jpg',
+        productImageAlt: 'Product 4',
     },
 ];
 
+// Carousel
+const carouselElements = {
+    carouselContainer: document.getElementById('carousel-container'),
+    carouselImage: document.querySelectorAll('.carousel-img-container img'),
+    carouselThumbnail: document.querySelectorAll('.carousel-imgs-small li img'),
+};
+
+// Quantity
 const quantityElements = {
     quantityContainer: document.querySelector('.product-infos-container'),
     quantityMinus: document.querySelector('#quantity #minus'),
     quantityPara: document.querySelector('#quantity span'),
     quantityPlus: document.querySelector('#quantity #plus'),
     warningPara: document.createElement('span'),
-}
+};
 
 
 
 /***EVEN LISTENERS***/
 
 // PRODUCT LIST
-const productList = document.querySelectorAll('#products-imgs-small li'); //Renvoie une NodeList, soit un tableau contenant tout les éléments lis.
+const productListThumbnails = document.querySelectorAll('.products-imgs-small li img');
+const productList = document.querySelectorAll('.products-imgs-small li'); //Renvoie une NodeList, soit un tableau contenant tout les éléments lis.
 
-productList.forEach(function(product) { // Je boucle sur mon tableau productList
+
+productList.forEach(function (product) { // Je boucle sur mon tableau productList
+
+    // J'ajoute les images et les alt aux balises li
+    for (let thumbnail = 0; thumbnail < productsImages.length; thumbnail++) {
+
+        productListThumbnails[thumbnail].setAttribute('src', productsImages[thumbnail].productThumbnailSrc);
+        productListThumbnails[thumbnail].setAttribute('alt', productsImages[thumbnail].productThumbnailAlt);
+    }
 
     // J'ajoute un évènement de click au produit
-    product.addEventListener('click', function() {
+    product.addEventListener('click', function () {
 
         /*
         - Ici la fonction removeClass va parcourir tout les éléments, lorsque je vais cliquer sur un élément, il prendra la classe
@@ -51,33 +78,86 @@ productList.forEach(function(product) { // Je boucle sur mon tableau productList
         */
         removeClass(); // J'appelle ma fonction removeClass()
         product.classList.add('active-style'); // Puis j'ajoute la classe à l'élément cliqué
+
+        makeCarouselVisible(); // J'appelle la fonction makeCarouselVisible pour l'afficher
     })
 });
 
+
+
+// removeClass()
 function removeClass() {
 
-    productList.forEach(function(product) { // Je parcours mon tableau productList
+    productList.forEach(function (product) { // Je parcours mon tableau productList (les balises li)
 
         product.classList.remove('active-style'); // Si l'un des produit a la classe 'active-style', je la supprime
     });
 }
 
 
+// CAROUSEL
+function makeCarouselVisible() {
+
+    // Images (grandes images)
+    for (let carouselImageSrc = 0; carouselImageSrc < productsImages.length; carouselImageSrc++) {
+
+        carouselElements.carouselImage[carouselImageSrc].setAttribute('src', productsImages[carouselImageSrc].productImageSrc);
+        carouselElements.carouselImage[carouselImageSrc].setAttribute('alt', productsImages[carouselImageSrc].productImageAlt);
+    }
+
+
+    // Thumbnails (petites images)
+    for (let carouselThumbnailSrc = 0; carouselThumbnailSrc < productsImages.length; carouselThumbnailSrc++) {
+
+        carouselElements.carouselThumbnail[carouselThumbnailSrc].setAttribute('src', productsImages[carouselThumbnailSrc].productThumbnailSrc);
+        carouselElements.carouselThumbnail[carouselThumbnailSrc].setAttribute('alt', productsImages[carouselThumbnailSrc].productThumbnailAlt);
+    }
+
+
+
+    //Je fais une boucle for pour parcourir le tableau (querySelectorAll / NodeList) et ajouter les images au carousel
+    for (let image = 0; image < carouselElements.carouselImg.length; image++) {
+
+        carouselElements.carouselImage[image].setAttribute('src', productsImages[image].productImageSrc);
+        carouselElements.carouselImage[image].setAttribute('alt', productsImages[image].productImageAlt);
+
+
+        for (let getImage = 0; getImage < productsImages.length; getImage++) {
+
+            if (productsImages[getImage] === carouselElements[image]) {
+
+                carouselElements.carouselImage[image].setAttribute('src', productsImages[image].productImageSrc);
+                carouselElements.carouselImage[image].setAttribute('alt', productsImages[image].productImageAlt);
+
+            } else {
+                console.log('err');
+            }
+        }
+    }
+
+    // Il faut que l'image affichée sur le carousel soit la même que la petite image sur laquelle j'ai cliqué (balise img)
+    // Exemple: Voir si la grande et la petite image font parties du même objet (si leur objet ont le même id ou index)
+    // console.log(productsImages[0].id);
+
+    carouselElements.carouselContainer.style.display = 'block';
+
+    // Code initial (for...in) 
+    /*for (const imgThumbnail in productsImages) {
+
+        carouselImg.setAttribute('src', `${productsImages[imgThumbnail].productThumbnailSrc}`);
+        carouselImg.setAttribute('alt', `${productsImages[imgThumbnail].productThumbnailAlt}`);
+
+        console.log(`Source: ${productsImages[imgThumbnail].productThumbnailSrc}`);
+        console.log(`Alt: ${productsImages[imgThumbnail].productThumbnailAlt}`);
+    }*/
+}
+
+
+
 // QUANTITY
 function quantity() {
 
     let count = 0;
-
-    quantityElements.quantityMinus.addEventListener('click', function () {
-
-        count -= 1;
-        quantityElements.quantityPara.textContent = count;
-
-        // Lorsque la quantité désirée pour l'article atteint zéro...
-        if (count === 0) {
-            quantityElements.quantityMinus.setAttribute('disabled', true); //Je déclare l'attribut 'disabled' à true au bouton
-        }
-    });
 
     // Augmente la quantité
     quantityElements.quantityPlus.addEventListener('click', function () {
@@ -93,6 +173,17 @@ function quantity() {
             quantityElements.warningPara.textContent = 'Limit 2 per customers.';
 
             quantityElements.quantityContainer.append(quantityElements.warningPara);
+        }
+    });
+
+    quantityElements.quantityMinus.addEventListener('click', function () {
+
+        count -= 1;
+        quantityElements.quantityPara.textContent = count;
+
+        // Lorsque la quantité désirée pour l'article atteint zéro...
+        if (count === 0) {
+            quantityElements.quantityMinus.setAttribute('disabled', true); //Je déclare l'attribut 'disabled' à true au bouton
         }
     });
 }
